cd940193af7aeccce521f5606d84e85bc2ce67f1
atam-git <atamchristianatam@gmail.com>
2025-11-24 09:12:04 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Index() {
-  return <div className="min-h-screen bg-white"></div>;
+  return <div className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>;
 }
